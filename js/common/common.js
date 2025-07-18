8ea80224bbce7b5ed887ec73fdd5a462c4c20cc4
Michel <kingproducts02@gmail.com>
2025-07-18 14:01:40 +0530

    Update common.js

--- js/common/common.js
+++ js/common/common.js
@@ -11,3 +11,18 @@ function toggleMenu() {
   const navLinks = document.getElementById("navLinks");
   navLinks.classList.toggle("show");
 }
+
+// Hide Header on Scroll Down, Show on Scroll Up
+let lastScrollY = window.scrollY;
+const header = document.getElementById('mainHeader');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > lastScrollY) {
+        // Scrolling down
+        header.style.top = '-100px'; // hide
+    } else {
+        // Scrolling up
+        header.style.top = '0'; // show
+    }
+    lastScrollY = window.scrollY;
+});
